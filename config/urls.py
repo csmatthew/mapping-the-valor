213026ef6b717ad5eee624f1e9710421cd605ec9
csmatthew <christopher.s.matthew@gmail.com>
2025-02-11 02:24:23 +0000

    Initial setup of ValorRecord model and basic configuration

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('', include('records.urls')),
+    path('records/', include('records.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
---
+++ records/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models.valor_record import ValorRecord
+
+# Register the model with the admin site
+admin.site.register(ValorRecord)
