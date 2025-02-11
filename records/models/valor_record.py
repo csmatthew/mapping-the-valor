56e0f43b894288e2c0163a1bdf35fa14c9aacfcd
csmatthew <christopher.s.matthew@gmail.com>
2025-02-11 02:58:19 +0000

    add content field to valor_record model

--- records/models/valor_record.py
+++ records/models/valor_record.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class ValorRecord(models.Model):
     name = models.CharField(max_length=255)
+    content = models.TextField(default='')
 
     def __str__(self):
         return self.name
