f842ee46b14d40f4b12206f91ee7b8a81b82a1a4
csmatthew <christopher.s.matthew@gmail.com>
2025-02-12 06:03:06 +0000

    add and test archdeaconry add works for admin

--- records/models/hierarchy.py
+++ records/models/hierarchy.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Province(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Diocese(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, unique=True)
     province = models.ForeignKey(Province, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Diocese(models.Model):
 
 
 class Archdeaconry(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, unique=True)
     diocese = models.ForeignKey(Diocese, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -25,7 +25,7 @@ class Archdeaconry(models.Model):
 
 
 class Deanery(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, unique=True)
     archdeaconry = models.ForeignKey(Archdeaconry, on_delete=models.CASCADE)
 
     def __str__(self):
