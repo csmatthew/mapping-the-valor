3b0885a4b3b67abbcc7a3675612d56f8a7822bd3
csmatthew <christopher.s.matthew@gmail.com>
2025-02-27 14:51:30 +0000

    Remove redundant features at this stage, check still works

--- static/js/map.js
+++ static/js/map.js
@@ -3,7 +3,6 @@ console.log('map.js loaded');
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM fully loaded and parsed');
     var mapContainer = document.getElementById('map');
-    var pinPlacementEnabled = false;
     var markers = []; // Array to store the markers
 
     if (mapContainer && !mapContainer._leaflet_map) {
@@ -77,45 +76,12 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
         // Handle overlay button click event
-        var mapOverlayButton = document.getElementById('map-overlay-button');
+        // var mapOverlayButton = document.getElementById('map-overlay-button');
 
-        mapOverlayButton.addEventListener('click', function () {
-            var mapOverlay = document.getElementById('map-overlay');
-            mapOverlay.style.display = 'none';
-        });
-
-        // Handle toggle pin placement button click event
-        var togglePinPlacementButton = document.getElementById('toggle-pin-placement');
-
-        togglePinPlacementButton.addEventListener('click', function () {
-            pinPlacementEnabled = !pinPlacementEnabled;
-            togglePinPlacementButton.textContent = pinPlacementEnabled ? 'Disable Pin Placement' : 'Enable Pin Placement';
-        });
-
-        // Handle map click event to place a pin
-        map.on('click', function (e) {
-            console.log('Map clicked:', e);
-            if (pinPlacementEnabled && document.getElementById('pin-form-container')) {
-                var latitude = e.latlng.lat;
-                var longitude = e.latlng.lng;
-
-                // Place a pin on the map
-                var marker = L.marker([latitude, longitude]).addTo(map);
-                console.log('Marker added at:', latitude, longitude);
-
-                // Fill the form with the pin location
-                document.getElementById('pin-latitude').value = latitude;
-                document.getElementById('pin-longitude').value = longitude;
-
-                // Show the form
-                document.getElementById('pin-form-container').style.display = 'block';
-
-                // Disable pin placement
-                pinPlacementEnabled = false;
-                togglePinPlacementButton.textContent = 'Enable Pin Placement';
-                console.log('Pin placement disabled');
-            }
-        });
+        // mapOverlayButton.addEventListener('click', function () {
+        //     var mapOverlay = document.getElementById('map-overlay');
+        //     mapOverlay.style.display = 'none';
+        // });
 
         // Handle monastery toggle button click event to toggle marker visibility
         var monasteryToggleButton = document.getElementById('monastery-toggle-button');
