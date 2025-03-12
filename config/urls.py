d11527ba2d3103706f0b27be271fb6c2dbf469c9
csmatthew <christopher.s.matthew@gmail.com>
2025-03-12 00:20:35 +0000

    restructure so home view is map

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from mapper.views import map_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', map_view, name='home'),
+    path('core/', include('core.urls')),  # not used at present
     path('map/', include('mapper.urls')),
     path('valor-records/', include('valor_records.urls')),
     path('accounts/', include('allauth.urls')),
